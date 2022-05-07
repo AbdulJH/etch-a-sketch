c04dd06580c1843d4a372bd41ff526a85056ae34
AbdulJH <abdul.chs21@gmail.com>
2022-05-07 12:43:06 -0500

    Created function to generate random color. When div is now hovered a random color is put in the square

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const container = document.querySelector(".container");
 
+
 function createGrid(rows, cols) {
   for (let i=0; i<(16*16); i++) {
-    const div = document.createElement("div");
-    div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
+    let div = document.createElement("div");
+    div.style.cssText = "border: 1px solid black; height: 25px; width: 25px;";
+    div.addEventListener("mouseover", function() {
+      div.style.backgroundColor = randomColor();
+    });
     container.appendChild(div);
+    
   }
+}
+function randomColor() {
+  const color = Math.floor(Math.random() * 16777216).toString(16);
+  return '#000000'.slice(0, -color.length) + color;
 
 }
+
 createGrid(8,8);
 
 
